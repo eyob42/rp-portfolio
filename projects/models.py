a152c0f8980cd137a5733a961452b9f210626269
eyob42 <eyobadigeh07@gmail.com>
2025-10-24 22:01:30 +0300

    Add Projects app with Project model and initial migration

---
+++ projects/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Projects(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=20)
