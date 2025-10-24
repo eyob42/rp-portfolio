0f57aa570892c17e5da9a3e0add2a696d61b10bb
eyob42 <eyobadigeh07@gmail.com>
2025-10-25 00:24:59 +0300

    Enable Django admin site and register Project model for easy portfolio management

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Projects
 
-# Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Projects, ProjectAdmin)
