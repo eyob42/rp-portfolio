73fc9c972cee2edb10b355703c14af2504966b3f
eyob42 <eyobadigeh07@gmail.com>
2025-10-25 02:38:06 +0300

    Update settings for production deployment

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -19,12 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#)^x(y)c!)p@s4)_@9x#zu$pc$iwc&n0$g8hq114$fslz9xva*'
+import os
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-#)^x(y)c!)p@s4)_@9x#zu$pc$iwc&n0$g8hq114$fslz9xva*')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
